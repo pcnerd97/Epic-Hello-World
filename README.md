5da8a5996343f16abd37d7e41294e5cdd2b2006c
Toby <tobyneil1@hotmail.co.uk>
2021-12-12 17:24:56 +0000

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![](https://repository-images.githubusercontent.com/437589831/638cc268-0e86-49bc-9f8b-6397129fef50)
+
 # Epic-Hello-World
  SFML Testing (Linux only for now)
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+/// EPIC HELLO WORLD
+/// By Toby Franklin
+//  Requires SFML and TGUI (Look in your distros repos)
+
 #include "PCN/Game.h"
 
 int main(int argc, char** argv)
